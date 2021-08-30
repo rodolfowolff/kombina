75fc811b58c441c36b69b78bab0b86123c44bffa
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-08-30 13:46:45 -0300

    add getStaticPaths

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,9 +1,36 @@
 import Layout from "@/components/Layout";
+import { API_URL } from "@/config/index";
 
-export default function EventsPage() {
+export default function EventsPage({ evt }) {
   return (
     <Layout>
-      <h1>Events page</h1>
+      <h1>{ evt.name }</h1>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(`${API_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((evt) => ({
+    params: { slug: evt.slug }
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const res = await fetch(`${API_URL}/api/events/${slug}`);
+  const events = await res.json();
+
+  return {
+    props: {
+      evt: events[0],
+    },
+    revalidate: 1
+  };
 }
