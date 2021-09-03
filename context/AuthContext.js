5f1e81cd8366a831f41d86b68061ed0c44e71afa
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-09-03 12:13:05 -0300

    add display events user for dashboard

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -33,17 +33,6 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  const logout = async () => {
-    const res = await fetch(`${NEXT_URL}/api/logout`, {
-      method: 'POST',
-    });
-
-    if (res.ok) {
-      setUser(null);
-      router.push('/');
-    }
-  };
-
   const login = async ({ email: identifier, password }) => {
     const res = await fetch(`${NEXT_URL}/api/login`, {
       method: 'POST',
--- pages/account/dashboard.js
+++ pages/account/dashboard.js
@@ -1,9 +1,44 @@
-import Layout from "@/components/Layout";
+import { parseCookies } from "@/helpers/index";
 
-export default function DashboardPage() {
+import Layout from "@/components/Layout";
+import DashboardEvents from "@/components/DashboardEvents";
+
+import { API_URL } from "@/config/index";
+
+import styles from "@/styles/Dashboard.module.css";
+
+export default function DashboardPage({ events }) {
+  const deleteEvent = () => {
+
+  };
+
   return (
     <Layout title='User dashboard'>
-      <h1>Dashboard</h1>
+      <div className={ styles.dash }>
+        <h1>Dashboard</h1>
+        <h3>My events</h3>
+
+        { events.map((evt) => (
+          <DashboardEvents key={ evt.id } evt={ evt } handleDelete={ deleteEvent } />
+        )) }
+      </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps({ req }) {
+  const { token } = parseCookies(req);
+
+  const res = await fetch(`${API_URL}/events/me`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  });
+
+  const events = await res.json();
+
+  return {
+    props: { events },
+  };
 }
