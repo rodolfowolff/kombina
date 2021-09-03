0b248120fd77da40f23a9f92ce09e7a02141437b
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-09-03 20:33:14 -0300

    git update

--- pages/404.js
+++ pages/404.js
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
     <Layout title="Page not found">
       <div className={ styles.error }>
         <h1><FaExclamationTriangle />404</h1>
-        <h4>Sorry, there is nothing here</h4>
+        <h4>Sorry, there is nothing here!</h4>
         <Link href="/">Go back home</Link>
       </div>
     </Layout>
